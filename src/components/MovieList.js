429a2a65d860aec72dd1c1d226aa731826c3d692
ebdem <ebu.bekir.3333@gmail.com>
2020-10-09 14:19:35 +0300

    add delete button and events

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,29 +1,43 @@
-import React from 'react'
+import React from "react";
 
- class MovieList extends React.Component {
-    render() {
-        return (
-            <div className='row'>
-                <div className='col-md-4' >
-                    <div className='card mb-4 shadow-sm'>
-                        <img 
-                        src='https://image.tmdb.org/t/p/w600_and_h900_bestv2/dXNAPwY7VrqMAo51EKhhCJfaGb5.jpg'
-                        className='card-img-top'
-                        alt='Sample Movie'/>
-                        <div className='card-body'>
-                            <h5 className='card-title'>Sample Movie</h5>
-                            <p className='card-text'>Sample Movie Text</p>
-                            <div className='d-flex justify-content-between  align-items-center'>
-                                <button type='button' className='btn btn-md btn-outline-danger'>DELETE</button>
-                                <h2><span className='badge badge-info'>9</span></h2>
-                            </div>
-                        </div>
-                    </div>
+const MovieList = (props) => {
+
+    // function handleClick(){ 
+    //     console.log('Buton Tıklandı')
+    // }
+ 
+    return (
+      <div className="row">
+        {props.movies.map((movie) => (
+          <div className="col-md-3" key={movie.id}>
+            <div className="card mb-4 shadow-sm">
+              <img
+                src={movie.imageURL}
+                className="card-img-top"
+                alt="Sample Movie"
+              />
+              <div className="card-body">
+                <h5 className="card-title">{movie.name}</h5>
+                <p className="card-text">{movie.overview}</p>
+                <div className="d-flex justify-content-between  align-items-center">
+                  <button
+                    type="button"
+                    onClick={(event) => props.deleteMovieProps(movie)}
+                    className="btn btn-md btn-outline-danger"
+                  >
+                    DELETE
+                  </button>
+                  <h2>
+                    <span className="badge badge-info">{movie.rating}</span>
+                  </h2>
                 </div>
-                
+              </div>
             </div>
-        )
-    }
+          </div>
+        ))}
+      </div>
+    );
+  
 }
 
 export default MovieList;
