9fade90de501c1b4f24ae2a062946a26689c02ff
ebdem <ebu.bekir.3333@gmail.com>
2020-10-13 14:34:50 +0300

    added addMovie component and we use routing with react-router-dom

--- src/components/App.js
+++ src/components/App.js
@@ -1,80 +1,77 @@
-import React from 'react';
-import SearchBar from './SearchBar';
-import MovieList from './MovieList';
-import axios from 'axios';
-
-
+import React from "react";
+import SearchBar from "./SearchBar";
+import MovieList from "./MovieList";
+import AddMovie from "./AddMovie";
+import axios from "axios";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 class App extends React.Component {
+  state = {
+    // npx json-server --watch src/api/movies.json --port 3002 --> json server izleme
+    //fetch asenkron olarak network sorguları yapmamızı sağlayan bir javascript fonksiyonu ve bize bir promise döner
+    //fetch yapıcaksak componentDidMount içinde yapmak en mantıklısı
+    movies: [],
 
-    state = {
-        // npx json-server --watch src/api/movies.json --port 3002 --> json server izleme
-        //fetch asenkron olarak network sorguları yapmamızı sağlayan bir javascript fonksiyonu ve bize bir promise döner
-        //fetch yapıcaksak componentDidMount içinde yapmak en mantıklısı 
-        movies: [],
+    search: "",
+  };
 
-        search:''
-    }
+  async componentDidMount() {
+    //axios http istekleri yapmak iiçin kullanılan promise tabanlı bir kütüphane
+    const response = await axios.get("http://localhost:3002/movies");
 
+    this.setState({ movies: response.data });
+  }
 
-   
+  //AXIOS API
+  deleteMovie = async (movie) => {
+    axios.delete(`http://localhost:3002/movies/${movie.id}`);
 
-    async componentDidMount(){
-        //axios http istekleri yapmak iiçin kullanılan promise tabanlı bir kütüphane
-        const response = await axios.get('http://localhost:3002/movies');
-        
-        this.setState({movies: response.data})
-    }
+    const newMovieList = this.state.movies.filter((m) => m.id !== movie.id);
 
-  
+    this.setState((state) => ({
+      movies: newMovieList,
+    }));
+  };
 
+  searchMovie = (event) => {
+    this.setState({ search: event.target.value });
+  };
 
-    
-
-    //AXIOS API
-    deleteMovie = async (movie) => {
-        axios.delete(`http://localhost:3002/movies/${movie.id}`)
-        
-        const newMovieList = this.state.movies.filter(
-            m => m.id !== movie.id
-        );
-       
-        this.setState(state => ({
-            movies:newMovieList
-        }))
-    }
-
-    searchMovie = (event) => {
-        this.setState({search:event.target.value})
-    }
-
-    render(){
-
-        let filteredMovies = this.state.movies.filter(
-            (movie) => {
-                return movie.name.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1
-            }
-        )
-        return(
-            <div className='container'>
-
-                <div className='row'>
-                    <div className='col-lg-12'>
-                        <SearchBar searchMovieProp={this.searchMovie}/>
+  render() {
+    let filteredMovies = this.state.movies.filter((movie) => {
+      return (
+        movie.name.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1
+      );
+    });
+    return (
+      <Router>
+        <div className="container">
+            <Switch>
+            <Route path='/' exact render={() => (
+                        <React.Fragment>
+                            <div className="row">
+                        <div className="col-lg-12">
+                        <SearchBar searchMovieProp={this.searchMovie} />
+                        </div>
                     </div>
-                    
-                </div>
-                <MovieList 
-                movies={filteredMovies}
-                deleteMovieProps= {this.deleteMovie}
-                
-                />
+                    <MovieList
+                        movies={filteredMovies}
+                        deleteMovieProps={this.deleteMovie}
+                    />
+                     
+            </React.Fragment>
+         )}> 
+         </Route>
+          
 
-                
-            </div>
-            
-        );
-    }
+
+        <Route path='/add' component={AddMovie} />  
+            </Switch>
+          
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -17,14 +17,22 @@ import React from 'react'
         return (
             <div>
                <form onSubmit={this.handleFormSubmit} className='form-row mb-5 mt-3'>
-                   <div className='col-12'>
+                   <div className='col-10'>
                        <input onChange={this.props.searchMovieProp}
                         type='text' className='form-control' 
                         placeholder='Search a Movie'
                         
                         />
                    </div>
-
+                    <div className='col-2'>
+                        <button 
+                        type='button'
+                        className='btn btn-md btn-danger'
+                        style={{float:'right'}}
+                        >
+                            Add Movie
+                        </button>
+                    </div>
                </form>
                 
             </div>
