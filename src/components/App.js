f6b51f7e0bc0c967495163d7be321bf144a4344e
ebdem <ebu.bekir.3333@gmail.com>
2020-10-13 13:34:13 +0300

    added readMe info

--- src/components/App.js
+++ src/components/App.js
@@ -17,59 +17,19 @@ class App extends React.Component {
     }
 
 
-    // async componentDidMount(){
-    //     const baseURL = 'http://localhost:3002/movies';
-    //     const response =await fetch(baseURL);
-    //     console.log(response);
-    //     const data =await response.json()
-    //     console.log(data)
-    //     this.setState({movies:data})
-    // }
+   
 
     async componentDidMount(){
         //axios http istekleri yapmak iiçin kullanılan promise tabanlı bir kütüphane
         const response = await axios.get('http://localhost:3002/movies');
-        //console.log(response);
+        
         this.setState({movies: response.data})
     }
 
-    // deleteMovie = (movie) => {
-    //     const newMovieList = this.state.movies.filter(
-    //         m => m.id !== movie.id
-    //     );
-    //     //eğer herhangi bir state nesnemiz olmasaydı bunu kullanabilirdik
-    //     // this.setState({
-    //     //     movies:newMovieList
-    //     // })
-        
-    //         //state nesnemiz olduğu için bunu kullanıyoruzzzzz güncelleme yapıyoruz
-    //     this.setState(state => ({
-    //         movies:newMovieList
-    //     }))
-    // }
+  
 
 
-    //FETCH API
-    // deleteMovie = async (movie) => {
-    //     const baseURL = `http://localhost:3002/movies/${movie.id}`;
-    //     await fetch(baseURL, {
-    //         //fetch de method kullanmamız gerekmez ise default olarak GET methodu olur
-    //         method:'DELETE'
-    //     })
-    //     const newMovieList = this.state.movies.filter(
-    //         m => m.id !== movie.id
-    //     );
-    //     //eğer herhangi bir state nesnemiz olmasaydı bunu kullanabilirdik
-    //     // this.setState({
-    //     //     movies:newMovieList
-    //     // })
-        
-    //         //state nesnemiz olduğu için bunu kullanıyoruzzzzz güncelleme yapıyoruz
-    //     this.setState(state => ({
-    //         movies:newMovieList
-    //     }))
-    // }
-
+    
 
     //AXIOS API
     deleteMovie = async (movie) => {
